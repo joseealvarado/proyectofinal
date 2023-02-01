a560420323fa9136f730449c0cac66f0eb0dd772
joseealvarado <josel.alvarado1111@gmail.com>
2023-01-31 22:28:48 -0300

    proyecto final

---
+++ curriculum.js
@@ -0,0 +1,12 @@
+
+document.getElementById("COLORVERDE").addEventListener("click", function() {
+    document.body.style.backgroundColor = "#00FF00";
+});
+
+document.getElementById("COLORBLANCO").addEventListener("click", function() {
+    document.body.style.backgroundColor = "#FFFFFF";
+});
+
+document.getElementById("COLORAZUL").addEventListener("click", function() {
+    document.body.style.backgroundColor = "#33FFFF";
+});
