273170be03dbf4391aa2fe064def1d0a202965a9
Jose Alvarado <124101115+joseealvarado@users.noreply.github.com>
2023-02-21 14:06:28 -0300

    Update curriculum.js

--- curriculum.js
+++ curriculum.js
@@ -1,12 +1,12 @@
 
 document.getElementById("COLORVERDE").addEventListener("click", function() {
-    document.body.style.backgroundColor = "#00FF00";
-});
-
-document.getElementById("COLORBLANCO").addEventListener("click", function() {
-    document.body.style.backgroundColor = "#FFFFFF";
-});
-
-document.getElementById("COLORAZUL").addEventListener("click", function() {
-    document.body.style.backgroundColor = "#33FFFF";
-});
+        document.body.style.backgroundColor = "#00FF00";
+    });
+    
+    document.getElementById("COLORNEGRO").addEventListener("click", function() {
+        document.body.style.backgroundColor = "#000000";
+    });
+    
+    document.getElementById("COLORROSA").addEventListener("click", function() {
+        document.body.style.backgroundColor = "#fa0491";
+    });
