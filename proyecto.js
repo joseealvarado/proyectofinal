9bcda556783d83c824baaf8744aacdd1ccbb953b
Jose Alvarado <124101115+joseealvarado@users.noreply.github.com>
2023-02-28 11:16:28 -0300

    Add files via upload

---
+++ proyecto.js
@@ -0,0 +1,16 @@
+
+function datos() {
+    location.href = "index.html"
+}
+    function habilidades() {
+        location.href = "habilidades.html"
+}
+function experiencia() {
+    location.href = "experiencias.html"
+}
+function estudio() {
+    location.href = "estudios.html"
+}
+function proyecto() {
+    location.href = "proyecto.html"
+}
